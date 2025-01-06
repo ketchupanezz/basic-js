bb6545f258565ada210434cf32c830f5d63708fe
ketchupanezz <cheezsauce@mail.ru>
2025-01-07 01:18:01 +0300

    feat: 5 functions

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  const newMembers = members
+      .filter(item => typeof item === 'string')
+      .map(name => name.trim()[0].toUpperCase());
+
+  return newMembers.sort().join('');
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length > 0) {
+    throw new Error('Invalid date!');
+  }
+
+  try {
+    const month = date.getMonth();
+
+    if (month >= 2 && month <= 4) {
+      return 'spring';
+    } else if (month >= 5 && month <= 7) {
+      return 'summer';
+    } else if (month >= 8 && month <= 10) {
+      return 'autumn';
+    } else {
+      return 'winter';
+    }
+  } catch (error) {
+    throw new Error('Invalid date!');
+  }
+ }
 
 module.exports = {
   getSeason
